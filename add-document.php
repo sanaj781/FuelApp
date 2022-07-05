c29c5d05e3f718563512b2c58fd870012030a594
sanaj781 <sanaj781@gmail.com>
2022-07-05 20:10:31 +0200

    adding invoice backend feature added

--- add-document.php
+++ add-document.php
@@ -1,43 +1,39 @@
 <div id="add-doc-wraper">
     <h5 class="text-center pb-4">Dodaj Nową Fakturę</h5>
-    <form class="row g-3">
+    <form class="row g-3" method="post" action="<?php add_invoice(); ?>" enctype="multipart/form-data">
+
         <div class="col-6">
             <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Przebieg">
         </div>
         <div class="col-6">
-            <label for="inputPassword2" class="visually-hidden">Przebieg</label>
-            <input type="number" class="form-control" id="inputPassword2" placeholder="64325">
+            <label for="inputOddometer" class="visually-hidden">Przebieg</label>
+            <input type="number" class="form-control" id="inputOddometer" name="oddometer" placeholder="64325">
         </div>
 
         <div class="col-6">
             <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Kwota Faktury">
         </div>
         <div class="col-6">
-            <label for="inputPassword2" class="visually-hidden">Kwota</label>
-            <input type="number" class="form-control" id="inputPassword2" placeholder="np. 250,43">
+            <label for="amount" class="visually-hidden">Kwota</label>
+            <input type="number" step="any" class="form-control" id="amount" name="amount" placeholder="np. 250.43">
         </div>
 
         <div class="mb-3">
-            <input class="form-control " type="file" id="formFile">
+            <input class="form-control " type="file" name="image" id="fileToUpload">
         </div>
+        <input type="hidden" id="position" name="position">
         <div class="col-12">
-            <button type="submit" class=" col-12 btn btn-primary mb-3">Wyślij Fakturę</button>
+            <input type="submit" value="Upload Image" name="submit" class="col-12 btn btn-primary mb-3">
         </div>
+
     </form>
 
 </div>
 
-
-
 <!-- Getting location -->
-<p>Click the button to get your coordinates.</p>
-
-<button onclick="getLocation()">Try It</button>
-
-<p id="demo"></p>
-
 <script>
-    var x = document.getElementById("demo");
+    window.onload = getLocation();
+    var x = document.getElementById("position");
 
     function getLocation() {
         if (navigator.geolocation) {
@@ -48,8 +44,8 @@
     }
 
     function showPosition(position) {
-        x.innerHTML = "Latitude: " + position.coords.latitude +
-            "<br>Longitude: " + position.coords.longitude;
+        x.value = position.coords.latitude +
+            " " + position.coords.longitude;
     }
 </script>
 <!-- End of getting location -->
--- back-end/functions.php
+++ back-end/functions.php
@@ -83,3 +83,118 @@ function get_cars()
     }
 }
 //End of Function for getting a all cars available for a choosen user
+
+//Function for adding new invoice into database
+function add_invoice()
+{
+    if (isset($_POST["submit"])) {
+        if (isset($_FILES['image'])) {
+            $errors = array();
+            $file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
+            $file_name = $_FILES['image'][0] . uniqid() . '.' . $file_ext;
+            $file_size = $_FILES['image']['size'];
+            $file_tmp = $_FILES['image']['tmp_name'];
+            $file_type = $_FILES['image']['type'];
+            $extensions = array("jpeg", "jpg", "png");
+
+            if (in_array($file_ext, $extensions) === false) {
+                $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+            }
+
+            if ($file_size > 2097152) {
+                $errors[] = 'File size must be excately 2 MB';
+            }
+
+            if (empty($errors) == true) {
+                move_uploaded_file($file_tmp, "uploads/" . $file_name);
+                date_default_timezone_set('Europe/Warsaw');
+
+                $date = date('Y-m-d H:i:s');
+                include 'db.php';
+                $query = "INSERT INTO Invoices (plik, user, date, oddometer, amount, location)
+                            VALUES(
+                            '" . $file_name . "',
+                            '" . $_SESSION['username'] . "',
+                             '" . $date . "',
+                             '" . $_POST['oddometer'] . "',
+                             '" . $_POST['amount'] . "',
+                             '" . $_POST['position'] . "'
+
+
+
+                            )";
+                $result = @mysqli_query($conn, $query);
+                if ($result) {
+                    echo json_encode(["sent" => 1,]);
+                } else echo json_encode(["sent" => 0,]);
+
+                echo "Success";
+            } else {
+                echo $errors[0];
+            }
+        }
+    }
+}
+
+
+    // // Check if image file is a actual image or fake image
+    // if (isset($_POST["submit"])) {
+    //     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    //     if ($check !== false) {
+    //         echo "File is an image - " . $check["mime"] . ".";
+    //         $uploadOk = 1;
+    //     } else {
+    //         echo "File is not an image.";
+    //         $uploadOk = 0;
+    //     }
+    // }
+
+    // // Check if file already exists
+    // if (file_exists($target_file)) {
+    //     echo "Sorry, file already exists.";
+    //     $uploadOk = 0;
+    // }
+
+    // // Check file size
+    // if ($_FILES["fileToUpload"]["size"] > 500000) {
+    //     echo "Sorry, your file is too large.";
+    //     $uploadOk = 0;
+    // }
+
+    // // Allow certain file formats
+    // if (
+    //     $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    //     && $imageFileType != "gif"
+    // ) {
+    //     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    //     $uploadOk = 0;
+    // }
+
+    // Check if $uploadOk is set to 0 by an error
+    // if ($uploadOk == 0) {
+    //     echo "Sorry, your file was not uploaded.";
+    //     // if everything is ok, try to upload file
+    // } else {
+    //     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    //         $date = date('Y-m-d H:i:s');
+    //         include 'db.php';
+    //         $query = "INSERT INTO invoices (user, przebieg, kwotaFaktury, plik, data, geolocation, jazda_prywatna)
+    //             VALUES(
+    //             '" . $_POST['oddometer'] . "',
+    //             '" . $_POST['amount'] . "',
+    //             '" . $target_file . "',
+    //             '" . $date . "',
+    //             '" . $_POST['position'] . "',
+    //             '" . $_POST['description'] . "',
+    //             'wyslano do wyceny'
+    //             )";
+    //         $result = @mysqli_query($conn, $query);
+    //         if ($result) {
+    //             echo json_encode(["sent" => 1,]);
+    //         } else echo json_encode(["sent" => 0,]);
+    //         echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+    //     } else {
+    //         echo "Sorry, there was an error uploading your file.";
+    //     }
+    // }
+//End of Function for adding new invoice into database
